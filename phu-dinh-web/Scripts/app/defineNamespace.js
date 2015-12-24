78ab6bbbe2f362539d52cc4d1cf61d2667848106
kudo183 <kudo183@gmail.com>
2015-12-24 09:06:41 +0700

    - add view utils for create common used view.

--- phu-dinh-web/Scripts/app/defineNamespace.js
+++ phu-dinh-web/Scripts/app/defineNamespace.js
@@ -1,3 +1,4 @@
 ﻿window.app = window.app || {};
 window.app.view = window.app.view || {};
+window.app.view.utils = window.app.view.utils || {};
 window.app.viewModel = window.app.viewModel || {};
---
+++ phu-dinh-web/Scripts/app/view/_utils.js
@@ -0,0 +1,27 @@
+﻿window.app.view.utils = (function () {
+    var utils = {
+        createReadOnlyGridView: createReadOnlyGridView
+    };
+    return utils;
+    
+    function createReadOnlyGridView(id, modelName) {
+        var gridViewModel = window.app.utilsDOM.createElement("div", {}, "with: gridViewModel");
+        var koItems = window.app.utilsDOM.createComment(" ko foreach: items ");
+        var row = window.app.utilsDOM.createElement("div", {}, "foreach: $parent.columns");
+        window.app.utilsDOM.addClass(row, "row");
+        var cell = window.app.utilsDOM.createElement("div", {});
+        window.app.utilsDOM.addClass(cell, "cell");
+        var cellText = window.app.utilsDOM.createElement("span", {}, "text: $parent[cellValueProperty]");
+        var endKoItems = window.app.utilsDOM.createComment(" /ko ");
+
+        gridViewModel.appendChild(koItems);
+        gridViewModel.appendChild(row);
+        row.appendChild(cell);
+        cell.appendChild(cellText);
+        gridViewModel.appendChild(endKoItems);
+
+        var view = window.app.utilsDOM.createElement("div", { id: id }, "with: " + modelName);
+        view.appendChild(gridViewModel);
+        return view;
+    };
+})();
