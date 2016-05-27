a872d3c832a8112c8abefb5ddca0ade1a0f2a077
kudo183 <kudo183@gmail.com>
2016-05-27 13:30:50 +0700

    - turn off EF lazy loading.

--- Data/PhuDinh.Context.cs
+++ Data/PhuDinh.Context.cs
@@ -18,6 +18,7 @@ namespace Data
         public PhuDinhEntities()
             : base("name=PhuDinhEntities")
         {
+            this.Configuration.LazyLoadingEnabled = false;
         }
     
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
--- Data/PhuDinh.Designer.cs
+++ Data/PhuDinh.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'D:\GitHub\phu-dinh-web\trunk\Data\PhuDinh.edmx'. 
+﻿// Default code generation is disabled for model 'D:\GitHub\phu-dinh-web\Data\PhuDinh.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
